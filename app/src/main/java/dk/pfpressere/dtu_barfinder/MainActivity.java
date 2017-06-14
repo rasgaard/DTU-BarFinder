01383ce6607219887b451c3d00df989caf5f46c1
Rasmus Aagaard <raagaard97@gmail.com>
2017-06-14 15:55:21 +0200

    Added logo to drawable folder

--- app/src/main/java/dk/pfpressere/dtu_barfinder/MainActivity.java
+++ app/src/main/java/dk/pfpressere/dtu_barfinder/MainActivity.java
@@ -1,7 +1,9 @@
 package dk.pfpressere.dtu_barfinder;
 
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.main_text_view);
+
     }
 }
