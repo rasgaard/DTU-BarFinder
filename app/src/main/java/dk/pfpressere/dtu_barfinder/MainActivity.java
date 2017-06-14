89a7152206a49c4f571b6aaff11590d00c10667a
Johnnyboi42 <johsgg@gmail.com>
2017-06-14 16:39:05 +0200

    Basic Google Maps navigation

--- app/src/main/java/dk/pfpressere/dtu_barfinder/MainActivity.java
+++ app/src/main/java/dk/pfpressere/dtu_barfinder/MainActivity.java
@@ -1,8 +1,12 @@
 package dk.pfpressere.dtu_barfinder;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = (TextView) findViewById(R.id.main_text_view);
 
+        Button mapsbutton = (Button) findViewById(R.id.vknapid);
+        mapsbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://maps.google.dk/maps?=34.34&daddr=55.7865880, 12.5253610"));
+                startActivity(intent);
+            }
+        });
+        // Christensen lol amiright
     }
 }
