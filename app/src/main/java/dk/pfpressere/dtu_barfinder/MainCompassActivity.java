fdb2c654d818a7a07042d82f9bb5e1513aa7cbd3
Rasmus Emil Christensen <s164408@student.dtu.dk>
2017-06-14 17:23:12 +0200

    Removed fap-button from and app_bar_main_compass.xml MainCompassActivity.java.

--- app/src/main/java/dk/pfpressere/dtu_barfinder/MainCompassActivity.java
+++ app/src/main/java/dk/pfpressere/dtu_barfinder/MainCompassActivity.java
@@ -1,7 +1,6 @@
 package dk.pfpressere.dtu_barfinder;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -22,15 +21,6 @@ public class MainCompassActivity extends AppCompatActivity
         setContentView(R.layout.activity_main_compass);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
