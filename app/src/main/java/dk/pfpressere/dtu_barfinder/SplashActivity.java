47d5618cf1c4f424b985b08ca3adf00e2ebe29ff
Rasmus Emil Christensen <s164408@student.dtu.dk>
2017-06-15 14:05:06 +0200

    The main_frame is now in direct control again!

--- app/src/main/java/dk/pfpressere/dtu_barfinder/SplashActivity.java
+++ app/src/main/java/dk/pfpressere/dtu_barfinder/SplashActivity.java
@@ -18,7 +18,7 @@ public class SplashActivity extends AppCompatActivity {
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent = new Intent(SplashActivity.this, MainCompassActivity.class);
                 startActivity(intent);
                 finish();
             }
