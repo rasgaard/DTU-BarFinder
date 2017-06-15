cfe13e8c7792d8eb28d611fa0f7b0d7015774379
Rasmus Emil Christensen <s164408@student.dtu.dk>
2017-06-15 11:43:40 +0200

    Added new first working button in the navigation activity.

--- app/src/main/java/dk/pfpressere/dtu_barfinder/MainActivity.java
+++ app/src/main/java/dk/pfpressere/dtu_barfinder/MainActivity.java
@@ -16,14 +16,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button mapsbutton = (Button) findViewById(R.id.vknapid);
-        mapsbutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://maps.google.dk/maps?=34.34&daddr=55.7865880, 12.5253610"));
-                startActivity(intent);
-            }
-        });
-
         Button startMainCompassActivityButton = (Button) findViewById(R.id.start_main_compass_activity_button_id);
         startMainCompassActivityButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -31,6 +23,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        // Christensen lol amiright
     }
 }
--- app/src/main/java/dk/pfpressere/dtu_barfinder/MainCompassActivity.java
+++ app/src/main/java/dk/pfpressere/dtu_barfinder/MainCompassActivity.java
@@ -1,5 +1,7 @@
 package dk.pfpressere.dtu_barfinder;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -30,6 +32,14 @@ public class MainCompassActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.getMenu().getItem(0).setChecked(true);
+
     }
 
     @Override
@@ -69,6 +79,7 @@ public class MainCompassActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
@@ -76,7 +87,11 @@ public class MainCompassActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_google_maps_route) {
+            // Start a goggle maps activity.
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://maps.google.dk/maps?=34.34&daddr=55.7865880, 12.5253610")));
+            // resets the checked item to the current fragment.
 
         } else if (id == R.id.nav_share) {
 
