19294f0db4bc7ca595818573e98ff6b57a5cf94f
Rasmus Aagaard <raagaard97@gmail.com>
2017-06-19 19:43:42 +0200

    various small changes

--- app/src/main/java/dk/pfpressere/dtu_barfinder/CompassController.java
+++ app/src/main/java/dk/pfpressere/dtu_barfinder/CompassController.java
@@ -1,13 +1,7 @@
 package dk.pfpressere.dtu_barfinder;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.icu.text.CompactDecimalFormat;
 import android.location.Location;
-import android.location.LocationManager;
-import android.net.UrlQuerySanitizer;
-import android.support.v4.app.ActivityCompat;
+
 
 public class CompassController {
     private Location currentLocation;
@@ -43,23 +37,6 @@ public class CompassController {
         setCurrentLocation(currentLocation);
         heading = 0;
     }
-
-    /*public static double bearing(double currentLat, double currentLng, double targetLat, double targetLng) {
-        double latitude1 = Math.toRadians(currentLat);
-        double latitude2 = Math.toRadians(targetLat);
-
-        double longitudeDiff = Math.toRadians(targetLng - currentLng);
-
-        double x = Math.sin(longitudeDiff) * Math.cos(latitude2);
-        double y = Math.cos(latitude1) * Math.sin(latitude2) - Math.sin(latitude1) * Math.cos(latitude2) * Math.cos(longitudeDiff);
-
-        return 360 - (Math.toDegrees(Math.atan2(y, x)) + 270) % 360;
-    }
-
-    public static double bearing(Location currentLocation, Location targetLocation) {
-        return bearing(currentLocation.getLatitude(), currentLocation.getLongitude(),
-                targetLocation.getLatitude(), targetLocation.getLongitude());
-    }*/
 }
 
 
--- app/src/main/java/dk/pfpressere/dtu_barfinder/MyLocationListener.java
+++
@@ -1,8 +0,0 @@
-package dk.pfpressere.dtu_barfinder;
-
-/**
- * Created by Aagaard on 19-06-2017.
- */
-
-class MyLocationListener {
-}
